1e44772e240fbbf55a15b380c45660cbccfe1239
Arnoldi, Loretta <loretta.arnoldi@nttdata.com>
2026-09-03 17:05:54 +0200

    fix: added link to documentation

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ai_chatbot/presentation/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+///Followed https://medium.com/codetodeploy/build-a-flutter-ai-chatbot-from-scratch-in-30-minutes-no-experience-needed-4ee0b5957862
 void main() async {
   // Assicura che i binding di Flutter siano pronti
   WidgetsFlutterBinding.ensureInitialized();
